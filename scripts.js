2f963e3395eac5ba89ce438202038cf1ad8c76d1
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-05-24 10:49:39 +0200

    added action types for count manipulation

--- scripts.js
+++ scripts.js
@@ -7,3 +7,36 @@
 const initialState = {
     count: 0,
 };
+
+// Action types
+  /**
+   * action type for incrementing the count.
+   * @type {string}
+   */
+  const INCREMENT = "INCREMENT";
+  
+  /**
+   * action type for decrementing the count.
+   * @type {string}
+   */
+  const DECREMENT = "DECREMENT";
+  
+  /**
+   * action type for resetting the count.
+   * @type {string}
+   */
+  const RESET = "RESET";
+  
+  /**
+   * action type for adding a value to the count.
+   * @type {string}
+   */
+  const ADD = "ADD";
+  
+  /**
+   * action type for subtracting a value from the count.
+   * @type {string}
+   */
+  const SUBTRACT = "SUBTRACT";
+  
+  
